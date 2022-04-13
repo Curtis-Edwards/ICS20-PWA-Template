3e4336312fe2a15dc3f2a1662ed4e1c171060920
Curtis-Edwards <99354904+Curtis-Edwards@users.noreply.github.com>
2022-04-13 13:27:56 -0400

    fixed code

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 // Created by: Curtis Edwards
-// Created on: Sep 2020
+// Created on: April 2022
 // This file contains the JS functions for index.html
 
 'use strict'
@@ -8,14 +8,11 @@
  * Check servie worker.
  */
  if (navigator.serviceWorker) {
-  navigator.serviceWorker.register("/ICS20-PWA-Template/sw.js", {
-    scope: "/ICS20-PWA-Template/",
+  navigator.serviceWorker.register("/ICS2O-UNIT-x-yy/sw.js", {
+    scope: "/ICS2O-UNIT-x-yy/",
   })
 }
 
 /**
- * This function displays an alert.
+ * This function displays an xxx.
  */
-function myButtonClicked() {
-  document.getElementById("hello-world").innerHTML = "<p>Hello, World!</p>"
-}
